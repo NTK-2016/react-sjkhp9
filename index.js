8dd9677a97e70a0c33fc5fb3d03f6eba6f981293
NTK-2016 <itprofessionalnitish@gmail.com>
2019-07-03 01:35:20 +0530

    1 file modified

--- index.js
+++ index.js
@@ -22,10 +22,38 @@ class App extends Component {
             <h2>{this.state.content}</h2>
               <h1>{this.props.headerProp}</h1>
             <h2>{this.props.contentProp}</h2>
+             <h3>Array: {this.props.propArray}</h3>
+              <h3>Bool: {this.props.propBool ? "True..." : "False..."}</h3>
+            <h3>Func: {this.props.propFunc(3)}</h3>
+            <h3>Number: {this.props.propNumber}</h3>
+            <h3>String: {this.props.propString}</h3>
+            <h3>Object: {this.props.propObject.objectName1}</h3>
+            <h3>Object: {this.props.propObject.objectName2}</h3>
+            <h3>Object: {this.props.propObject.objectName3}</h3>
         </p>
       </div>
     );
   }
 }
-
+App.propTypes = {
+   propArray: React.PropTypes,
+     propBool: React.PropTypes,
+   propFunc: React.PropTypes,
+   propNumber: React.PropTypes,
+   propString: React.PropTypes,
+   propObject: React.PropTypes
+}
+App.defaultProps = {
+   propArray: [1,2,3,4,5],
+    propBool: true,
+   propFunc: function(e){return e},
+   propNumber: 1,
+   propString: "String value...",
+   
+   propObject: {
+      objectName1:"objeeectValue1",
+      objectName2: "objewrqrctValue2",
+      objectName3: "objectdsffValue3"
+   }
+}
 render(<App headerProp = "Header from props..." contentProp = "Content from props..."/>, document.getElementById('root'));
